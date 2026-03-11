9496283c876443877260e1793a572c2bc86156ea
Michael S. Taylor <mtaylor@semo.edu>
2026-03-11 15:59:42 -0500

    Time and date info suitably organized. I think.

--- summarize.R
+++ summarize.R
@@ -14,12 +14,15 @@
 library(here)
 library(tidyverse)
 
+# Constants
+time_zone = "Etc/GMT+6"
 
 # File inputs -------------------------------------------------------------
 
 # Import data files
 files <- fs::dir_ls(path = "data", glob = "*HawkEars.txt")
 
+# Use tmp_data for initial wrangling
 tmp_data <- read_tsv(
   files, 
   id = "path",
@@ -77,23 +80,58 @@ tmp_data <- tmp_data |> separate_wider_delim(code_confidence,
 )
 
 # Create a POSIX time stamp for each recording based on date and time
-# that each new audio file was started.
+# that each new audio file was started. Use "Etc/GMT+6" OlsonCode
+# to set consistent time zone independent of daylight savings time.
 tmp_data <- tmp_data |> 
   unite(date_time, c("date", "file_start_time"), sep = "") |> 
-  mutate(file_start_time = ymd_hms(date_time, tz = "America/Chicago"))
+  mutate(file_start_time = ymd_hms(date_time, tz = time_zone))
 
 # Calculate the relative start time and duration for each detection. This
 # should make it easier to find in the audio file when looking at 
 # spectrograms.
 
-# Save the result in the final data set.
-hawk_data <- tmp_data |> 
-  mutate(detection_start_time = file_start_time + as.period(start_time),
-         detection_end_time = file_start_time + as.period(end_time),
-         detection_duration = seconds(interval(start = detection_start_time, end = detection_end_time)))
+tmp_data <- tmp_data |>
+  mutate(
+    detection_start_time = file_start_time + as.period(start_time),
+    detection_end_time = file_start_time + as.period(end_time),
+    detection_duration = seconds(
+      interval(start = detection_start_time, end = detection_end_time)
+    ),
+    date = as_date(file_start_time),
+    recording_time = hms::as_hms(file_start_time),
+    detect_date = as_date(file_start_time),
+    detect_time = hms::as_hms(detection_start_time)
+  )
+
+# hawk_data <- hawk_data |> 
+#   mutate(date = as_date(file_start_time),
+#          recording_time = hms::as_hms(file_start_time),
+#          detect_date = as_date(file_start_time),
+#          detec_time = hms::as_hms(detection_start_time))
+
+# Be sure to run sunriset.R before this point.
+
+hawk_data <- left_join(
+  x = tmp_data,
+  y = sun_df,
+  by = "date"
+)
 
 
+rm(tmp_data)
 
+hawk_data <- hawk_data |> 
+  mutate(rise = hms::as_hms(sunrise),
+         set = hms::as_hms(sunset))
+
+hawk_data <- hawk_data |>
+  mutate(
+    time_since_sunrise = difftime(
+      time1 = detection_start_time,
+      time2 = sunrise,
+      tz = time_zone,
+      units = "mins")
+    )
 
 hawk_summary <- hawk_data |> 
   group_by(recorder, sp_code) |> 
